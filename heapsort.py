710d75cce03c150c33db74a89b40807db2c597d0
Janette Siu <janette@cs.washington.edu>
2012-10-18 11:26:25 -0700

    implemented heapsort

--- heapsort.py
+++ heapsort.py
@@ -1,1 +1,11 @@
 # Heap sort
+
+def sort(urls):
+    size = len(urls)
+    heap = []
+    for url in urls:
+        heappush(heap, url)
+        urls.pop(0)
+    for i in range(size):
+        urls.insert(0, heappop(heap))
+    return
