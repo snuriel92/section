4e8d09baf4098e0583be006cb9e1034796c338fa
Conor Reisman <creisman@cs.washington.edu>
2012-10-18 22:44:03 -0700

    Fixed a bug with mutating the original list in radixsort. Added test_input.txt, a list of 1000 randomly generated strings and test_output.txt, the solution.

--- algorithms/radixsort.py
+++ algorithms/radixsort.py
@@ -1,15 +1,16 @@
 # Radix sort
 
 def sort(urls):
-    size = len(urls)
-    maxlen = max(map(lambda url: len(url), urls))
+    temp = list(urls)
+    maxlen = max(map(lambda url: len(url), temp))
     for i in range(maxlen, -1, -1):
-      print i
       chars =  [[] for j in range (0, 256)] # Create a bucket for each char.
-      for url in urls:
+      for url in temp:
           # Grab the char or the 0 char if the string is ended.
           char = url[i] if i < len(url) else '\0'
           chars[ord(char)].append(url)
-      urls = [item for sublist in chars for item in sublist] # flatten
-    print urls
+      temp = [item for sublist in chars for item in sublist] # flatten
+    # Copy the values back into the original list.
+    for i in range(0, len(urls)):
+      urls[i] = temp[i]
     return urls
